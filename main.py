2228d4c96dc1c08a700a4ddcd1be8fea6a9ca633
Jeremy Thomas <jthomas@smoothfusion.com>
2020-10-01 19:36:23 -0500

    carrier merge done, fixed deltas

--- main.py
+++ main.py
@@ -11,20 +11,22 @@ stockFiles = ["AAL - Stock price.csv",
               "LUV - Stock Price.csv",
               "UAL - Stock Price.csv"]
 
-carrierFile = "US_CARRIER_SET.csv"
-countyFile = "pres16results.csv"
+carrierFile = "AirlineCityCountyFIPS.csv"
+electionFile = "pres16results.csv"
 covidFile = "us_counties_covid19_daily.csv"
 
 def main():
     mergedStonks = stonks.ReadAndMerge(inpath, stockFiles)
-    electionFrame = drumpf.Clean(pd.read_csv(inpath + countyFile))
+    electionFrame = drumpf.Clean(pd.read_csv(inpath + electionFile))
     covidFrame = pd.read_csv(inpath + covidFile, parse_dates=[0])
     carrierFrame = planes.Clean(pd.read_csv(inpath + carrierFile))
     firstMerge = plague.CleanAndMerge(covidFrame, electionFrame)
     secondMerge = firstMerge.merge(mergedStonks, left_on='date', right_on='Date', how='left')
     secondMerge.drop(columns=['Date'], inplace=True)
-    secondMerge.sort_values(by=['date', 'fips'], inplace=True)
     secondMerge.fillna(method='ffill', inplace=True)
-    secondMerge.to_csv(outpath +  "SecondTry.csv", index=False)
+    thirdMerge = secondMerge.merge(carrierFrame, on='date')
+    deltaFrame = plague.EncodeNewColumns(thirdMerge)
+    deltaFrame.sort_values(by=['date', 'fips'], inplace=True)
+    deltaFrame.to_csv(outpath + "Final.csv", index=False)
   
 main()
