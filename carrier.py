e8508621a9d5b66f8b247340b8f07fe8292795fc
Jeremy Thomas <jthomas@smoothfusion.com>
2020-10-02 21:20:40 -0500

    breaking changes for flight colors, DO NOT MERGE

--- carrier.py
+++ carrier.py
@@ -26,21 +26,28 @@ def Clean(df):
     newDf.dropna(inplace=True)
     newDf['DEST_FIPS'] = newDf['DEST_FIPS'].astype('int64')
     newDf['ORIGIN_FIPS'] = newDf['ORIGIN_FIPS'].astype('int64')
-    return Upsample(newDf)
+    return newDf
     
 def Upsample(df):
-    flights_by_month = df[['PASSENGERS', 'MONTH', 'CARRIER_NAME']].groupby('CARRIER_NAME')
+    flights_by_month = df[['PASSENGERS', 'MONTH', 'CARRIER_NAME', 'flight_color']].groupby('CARRIER_NAME')
     final = pd.DataFrame()
     for key, item in flights_by_month:
-        fieldName = str(item['CARRIER_NAME'].iloc[0] + 'avgPassengers')
+        passFieldName = str(item['CARRIER_NAME'].iloc[0] + '_avg_passengers')
         ranges = pd.to_datetime(['1/1/2020','2/1/2020', '3/1/2020', '4/1/2020', '5/1/2020', '6/1/2020', '6/30/2020'])
         newDf = pd.DataFrame({ 'date': ranges })
         newDf.set_index(ranges, inplace=True)
-        newDf[fieldName] = np.nan
-        group = item.groupby(['MONTH'])['PASSENGERS']
-        for innerKey, innerItem in group:
-            newDf[fieldName][innerKey-1 % 7] = innerItem.mean()
-        upsampled = newDf[fieldName].resample('D').mean()
-        final[fieldName] = upsampled.interpolate(method='spline', order=2)
+        newDf[passFieldName] = np.nan
+        passGroup = item.groupby(['MONTH'])
+        for innerKey, innerItem in passGroup:
+            newDf[passFieldName][innerKey-1 % 7] = innerItem['PASSENGERS'].mean()
+            innerGroup = innerItem.groupby(['flight_color'])
+            for innestKey, innestItem in innerGroup:
+                print(innerGroup.count())
+                colorFieldName = passFieldName + str(innestItem['flight_color'].iloc[0])
+                newDf[colorFieldName] = np.nan
+                newDf[colorFieldName][innerKey-1 % 7] = innestItem['PASSENGERS'].mean()
+        upsampled = newDf[passFieldName].resample('D').mean()
+        final[passFieldName] = upsampled.interpolate(method='spline', order=2)
     final['date'] = final.index
+    print(final.count())
     return final
--- election.py
+++ election.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def Clean(df):
     df['fips'] = pd.to_numeric(df['fips'], errors='coerce')
@@ -11,5 +12,19 @@ def EncodeNewColumns(df):
     # Since Trump won, red will be 1 and blue will be 0
     df['color'] = df['lead'].apply(lambda x: 1 if 'Trump' in x else 0)
     return df.groupby(['fips', 'color']).size().reset_index()[['fips', 'color']]
+
+# blue is still 0, red is still 1, and mixed flights will be 2.
+# mixed is a flight with different color origin and destinations
+def ColorFlights(carrierFrame, electionFrame):
+    carrierFrame.reset_index(inplace=True, drop=True)
+    originMerge = carrierFrame.merge(electionFrame, how='left', left_on='ORIGIN_FIPS', right_on='fips')
+    carrierFrame['origin_color'] = originMerge['color'][originMerge['fips'] == carrierFrame['ORIGIN_FIPS']]
+    destMerge = carrierFrame.merge(electionFrame, how='left', left_on='DEST_FIPS', right_on='fips')
+    carrierFrame['dest_color'] = destMerge['color'][destMerge['fips'] == carrierFrame['DEST_FIPS']]
+    carrierFrame['flight_color'] = 2
+    carrierFrame['flight_color'][carrierFrame['dest_color'] == carrierFrame['origin_color']] = carrierFrame['dest_color']
+    carrierFrame.dropna(inplace=True)
+    carrierFrame.drop(columns=['origin_color', 'dest_color'], inplace=True)
+    return carrierFrame
     
     
